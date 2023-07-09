3db1a13beda8294c01db26cc6714d7125fd2d699
rafaa1999 <raferhouma10@gmail.com>
2023-07-09 16:46:26 +0100

    add light and dark theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="app">
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+        <main className="content">
+          
+        </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
