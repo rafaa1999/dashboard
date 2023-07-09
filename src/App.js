7b7ccc6d0270c581b7414d0ea67a7c2f99f4abbe
rafaa1999 <raferhouma10@gmail.com>
2023-07-09 17:19:06 +0100

    add tapbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
+import TopBar from "./scenes/global/Topbar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -9,7 +10,7 @@ function App() {
         <CssBaseline />
         <div className="app">
         <main className="content">
-          
+         <TopBar/> 
         </main>
         </div>
       </ThemeProvider>
